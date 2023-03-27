2b9d79ff1d86d30ff5de7cf09adc6ef530604448
Jake Crane <jakecrane91@gmail.com>
2023-03-27 09:41:26 -0500

    Moved styles to scss file and added Toggle Fix button

--- src/MyModal.tsx
+++ src/MyModal.tsx
@@ -1,50 +1,15 @@
 import {
     Button,
-    Col,
     Container,
-    FormFeedback,
-    FormGroup,
-    Input,
-    Label,
     Modal,
     ModalBody,
     ModalFooter,
     ModalHeader,
     Row
 } from "reactstrap";
-import { css } from "@emotion/react";
+import { useState } from "react";
 
 const isIframed = true; //window.parent !== window;
-
-const iframedModalCss = css`
-    /* CSS specific to iOS devices */
-    @supports (-webkit-touch-callout: none) {
-        flex-direction: column;
-        justify-content: end;
-        height: 100%;
-        min-height: calc(100% - 3.5rem);
-
-        .scroll-and-read-hint {
-            display: block !important;
-        }
-
-        /* Limit the modal height on medium sized devices such as ipads */
-        @media screen and (max-width: 820px), screen and (max-height: 820px) {
-            .modal-body {
-                max-height: 600px;
-                background-color: blue;
-            }
-        }
-
-        /* Limit the modal height on smaller devices such as ihphones */
-        @media screen and (max-width: 480px), screen and (max-height: 480px) {
-            .modal-body {
-                max-height: 400px;
-                background-color: red;
-            }
-        }
-    }
-`;
 
 interface ComponentProps {
     toggle: () => void;
@@ -57,13 +22,14 @@ const MyModal = ({
     achTextAgreement,
     agreed = false
 }: ComponentProps) => {
+    const [useFix, setUseFix] = useState(true)
     // const ssvPrompt = false;
     return (
         <Modal
             isOpen
             size="lg"
             scrollable
-            css={isIframed ? iframedModalCss : undefined}
+            className={(useFix && isIframed) ? 'iframed-modal' : undefined}
         >
             <ModalHeader toggle={toggle} tag="h4">
                 Terms and Conditions
@@ -73,7 +39,7 @@ const MyModal = ({
                     <Row className="mb-2 d-none scroll-and-read-hint">
                         <strong>Please scroll to read and accept the agreement.</strong>
                     </Row>
-                    <div dangerouslySetInnerHTML={{ __html: achTextAgreement ?? "" }} />
+                    <div className="row" dangerouslySetInnerHTML={{ __html: achTextAgreement ?? "" }} />
                     {/* {!agreed && ssvPrompt && (
                   <FormGroup row>
                       <Label xs={6} for="ssvResponse">
@@ -117,6 +83,7 @@ const MyModal = ({
                 ) : (
                     <Button color="primary">Agree</Button>
                 )}
+                <Button color="primary" onClick={() => setUseFix(prev => !prev)}>Toggle Fix</Button>
             </ModalFooter>
         </Modal>
     );
